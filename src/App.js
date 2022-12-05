d06c20411a3d549597934595be5709939e56d96e
CSColaneri <63002450+CSColaneri@users.noreply.github.com>
2022-12-05 16:44:13 -0500

    Some broken history code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore/lite';
+import { doc, setDoc, Timestamp } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,11 +25,38 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-async function getRestaurants(db) {
+async function getRestaurants() {
   const restaurantsCol = collection(db, 'restaurants');
   const restaurantsSnapshot = await getDocs(restaurantsCol);
   const restaurantsList = restaurantsSnapshot.docs.map(doc => doc.data());
   return restaurantsList;
+}
+
+async function rateRestaurant(restObject, restRating){
+  let text1 = "users/";
+  let text2 = 'i7Bs7uChMXB5oacg0Dln' + '/history/';
+  let finalText = text1.concat(text2);
+  console.log(finalText);
+  const historyItem = {
+    dateAdded: Timestamp.now(),
+    rating: restRating,
+    restaurant: restObject
+  };
+  const myArray = restObject.split("/");
+  let word = myArray[2];
+  console.log(finalText.concat(word));
+  const location = doc(db, finalText.concat(word));
+  console.log("after doc");
+  setDoc(location, historyItem);
+  console.log("After setDoc");
+}  
+
+async function getHistory()
+{
+  const historyCol = collection(db, 'i7Bs7uChMXB5oacg0Dln' + '/' + 'history');
+  const historySnapshot = await getDocs(historyCol);
+  const histList = historySnapshot.docs.map(doc => doc.data());
+  return histList;
 }
 
 // skeleton project
@@ -52,5 +80,7 @@ function App() {
     </div>
   );
 }
+
+rateRestaurant("/restaurants/1", 0);
 
 export default App;
