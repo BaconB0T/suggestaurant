3ad8384d91a7a7c2bc8bb1f7f6057871bee0b19a
Christian Previtali <PrevitaliCA18@gcc.edu>
2022-12-07 14:47:38 -0500

    Make History Search
    Can search but not add restaurant to history

---
+++ src/components/HistorySearch.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from "react";
+import{db, getAllRestaurants} from "../firestore"
+// import SearchIcon from "@material-ui/icons/Search";
+// import CloseIcon from "@material-ui/icons/Close";
+
+export default function HistorySearch() {
+    const[getRest, setGetRest] = useState([]);
+
+    console.log(getRest);
+    useEffect(() => {
+        async function getRes() {
+            const resta = await getAllRestaurants();
+            setGetRest(resta);
+        }
+
+        getRes();
+    }, []);
+
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = getRest.filter((value) => {
+          return value.name.toLowerCase().includes(searchWord.toLowerCase());
+        });
+    
+        if (searchWord === "") {
+          setFilteredData([]);
+        } else {
+          setFilteredData(newFilter);
+        }
+      };
+
+      const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+      };
+    
+    return (
+        <div className="search">
+          <div className="searchInputs">
+            <input
+              type="text"
+              placeholder={"Enter a Restaurant"}
+              value={wordEntered}
+              onChange={handleFilter}
+            />
+            {/* <div className="searchIcon">
+              {filteredData.length === 0 ? (
+                <SearchIcon />
+              ) : (
+                <CloseIcon id="clearBtn" onClick={clearInput} />
+              )}
+            </div> */}
+          </div>
+          {filteredData.length != 0 && (
+            <div className="dataResult">
+              {filteredData.slice(0, 15).map((value, key) => {
+                return (
+                  <a className="dataItem" target="_blank">
+                    <p>{value.name} </p>
+                  </a>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      );
+}
