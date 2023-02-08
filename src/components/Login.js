a1a37642917677a20280fa564c1fd2e002505b4a
@EthanStyle <brownev19@gcc.edu>
2023-02-07 20:52:24 -0500

    Update Login.js
    Follows new sign in flow, cookies may go at some point, but not for now at least.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,73 +3,69 @@ import { Container, Card, Form, Button, Alert } from 'react-bootstrap'
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { getAccount, validateUser } from '../firestore'
+import { getAccount, validateUser, signInEmailPassword } from '../firestore'
 
 const Login = () => {
     const [cookies, setCookie] = useCookies(['user']);
     const navigate = useNavigate();
-    const usernameRef = useRef()
-    const passwordRef = useRef()
-    const [error, setError] = useState("")
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState("");
 
     async function handleSubmit(e) {
         e.preventDefault(); // don't refresh the page
-        try {
-            setError("")
-            // if successful login...
-            if (await validateUser(usernameRef.current.value, passwordRef.current.value) === false) {
-                console.log("UNSUCCSSFUL");
-                throw ("Invalid username and password combination!");
-            }
-            const user = await getAccount("username", usernameRef.current.value);
-            console.log(user);
-            console.log(user.id);
+        setError("");
+        signInEmailPassword(usernameRef.current.value, passwordRef.current.value)
+            .then((bool, idOrCode) => {
+                if (bool) {
+                    setCookie('Name', usernameRef.current.value, { path: '/' });
+                    setCookie('id', idOrCode, { path: '/' });
+                    navigate("/accounts");
+                } else {
+                    setError("Invalid email and password combination.");
+                }
+            });
+        // if successful login...
+        // if (validateUser(usernameRef.current.value, passwordRef.current.value) === false) {
+        //     console.log("UNSUCCSSFUL");
+        //     throw ("Invalid username and password combination!");
+        // }
+        // const user = await getAccount("username", usernameRef.current.value);
+        // console.log(user);
+        // console.log(user.id);
 
-            console.log("SUCCESS");
-            
+        // console.log("SUCCESS");
 
-            setCookie('Name', usernameRef.current.value, { path: '/' });
-            setCookie('id', user.id, { path: '/' });
-            setCookie('Password', passwordRef.current.value, { path: '/' });
-            navigate("/accounts");
-        } catch (e) {
-            // else set an error
-            setError(e)
-        }
+
     }
 
     return (
         <Container
-
             className="d-flex align-items-center justify-content-center"
-            style={{ minHeight: "100vh" }}
-        >
+            style={{ minHeight: "100vh" }}>
             <div className="w-100" style={{ maxWidth: "400px" }}>
-                <>
-                    <Card>
-                        <Card.Body>
-                            <h2 className="text-center mb-4">Login</h2>
-                            {error && <Alert variant="danger">{error}</Alert>}
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group id="username" className="mb-2">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control ref={usernameRef} required />
-                                </Form.Group>
-                                <Form.Group id="password" className="mb-2">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" ref={passwordRef} required />
-                                </Form.Group>
-                                <Button className="w-40 mt-10" type="submit">
-                                    Login
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                    <div className="w-100 text-center mt-2">
-                        Need an account? <Link to="/accounts">Sign Up</Link>
-                    </div>
-                </>
-
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Login</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group id="username" className="mb-2">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control ref={usernameRef} required />
+                            </Form.Group>
+                            <Form.Group id="password" className="mb-2">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required />
+                            </Form.Group>
+                            <Button className="w-40 mt-10" type="submit">
+                                Login
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                    Need an account? <Link to="/signup">Sign Up</Link>
+                </div>
             </div>
         </Container >
 
