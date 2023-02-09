c588dfd344eba18ecda9a28c3731b539d86993d7
EthanStyle <ethan@styleriser.com>
2023-02-09 16:21:34 -0500

    Update Accounts.js
    Must be logged in to view

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { signOutUser, auth, deleteUser } from "../firestore";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, Navigate } from 'react-router-dom';
 import { Alert, Button } from "react-bootstrap";
 
 const DeleteAlert = (props) => {
@@ -27,44 +27,43 @@ const Account = () => {
   const user = auth.currentUser;
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
-
-  const sOut = () => {
-    signOutUser().then((res) => {
-      if(res) {
-        navigate('/');
-      } else {
-        alert("Something went wrong!")
-      }
-    });
-  }
-
-  const del = () => {
-    if(deleteUser()) {
-      navigate('/login');
-    } else {
-      navigate('/login');
+  if(auth.currentUser === null) {
+    return (
+        <Navigate to='/login' />
+    );
+  } else {
+    const sOut = () => {
+      signOutUser().then((res) => {
+        if(res) {
+          navigate('/');
+        } else {
+          alert("Something went wrong!")
+        }
+      });
     }
+  
+    return (
+      <div display='block'>
+        <DeleteAlert alertCallback={() => {deleteUser(); navigate('/login');}} show={show} setShow={setShow}></DeleteAlert>
+        <h1>Hello {user && user.username}</h1>
+        <div>Email: {user && user.email}</div>
+        <Link to='/account/allergies'>Allergies</Link>
+        <br></br>
+        <Link to='/account/filters'>Filters</Link>
+        <br></br>
+        <Link to='/account/history'>History</Link>
+        <br></br>
+        <button>
+          <Link to='/change-password'>Change Password</Link>
+        </button>
+        <br></br>
+        <button onClick={sOut}>Sign Out</button>
+        <br></br>
+        <button onClick={() => {setShow(true)}}>Delete Account (disabled)</button>
+        <br></br>
+      </div>
+    );
   }
-
-  return (
-    <div display='block'>
-      <DeleteAlert alertCallback={del} show={show} setShow={setShow}></DeleteAlert>
-      <h1>Hello {user && user.username}</h1>
-      <div>Email: {user && user.email}</div>
-      <Link to='/account/allergies'>Allergies</Link>
-      <br></br>
-      <Link to='/account/filters'>Filters</Link>
-      <br></br>
-      <Link to='/account/history'>History</Link>
-      <br></br>
-      <button>Change Password (disabled)</button>
-      <br></br>
-      <button onClick={sOut}>Sign Out</button>
-      <br></br>
-      <button onClick={() => {setShow(true)}}>Delete Account (disabled)</button>
-      <br></br>
-    </div>
-  );
 }
 
 export default Account
