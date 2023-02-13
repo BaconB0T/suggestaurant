d30775af4f6a2f9d3c26910b0626e18c670ae5a6
Christian Previtali <PrevitaliCA18@gcc.edu>
2023-02-13 02:01:18 -0500

    allergies with styling
    Incomplete allergies with styling

--- src/components/Allergies.jsx
+++ src/components/Allergies.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import { ButtonGroup } from "react-bootstrap";
 import {auth, getFilters, getDietRest, updateDietRestrictions} from "../firestore";
+import "../styles/Allergies.css";
 
 function Allergies(){
 
@@ -8,6 +9,7 @@ function Allergies(){
 
     const[dietRestList, setRestList] = useState([]);
     const[usersDietRest, setUserDietRest] = useState([]);
+    const[selected, setSelected] = useState("not-selected");
     const [t, setT] = useState(false);
 
     if(!t){
@@ -19,7 +21,18 @@ function Allergies(){
         });
         setT(true);
     }
-    
+
+    // const isSelected = () => {
+    //     if (usersDietRest.includes(alergy)) 
+    // }
+
+    async function isSelected(elem) {
+        if (usersDietRest.includes(elem)){
+            setSelected("selected")
+        }
+        else{setSelected('not-selected')}
+    }
+    // const [sel, setSel] = useState('not-selected');
     return(
         <div>
             <h1>Dietary Restrinctions</h1>
@@ -27,6 +40,9 @@ function Allergies(){
                 {dietRestList.map(alergy => {
                     return (
                         <button
+                        type = 'button'
+                        key = {alergy}
+                        id = {(usersDietRest.includes(alergy)) ? 'selected' : 'not-selected'}
                         onClick = {() => {
                             let tempList = usersDietRest;
                             if (!(tempList.includes(alergy))){
